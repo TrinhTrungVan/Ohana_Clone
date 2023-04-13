eb318fc6069f5469111542364c1df44883c83132
quyenptitptit <quyenmeomeo111@gmail.com>
2023-04-13 22:47:19 +0700

    fix authen 2nd

--- frontend/src/redux/apiRequest.js
+++ frontend/src/redux/apiRequest.js
@@ -1,11 +1,8 @@
-import { loginFailed, loginStart, loginSuccess, registerFailed, registerStart, registerSuccess } from "./authSlice";
-import { logoutFailed, logoutStart, logoutSuccess } from "./slices/authSlice";
-import { updateUserSuccess } from "./userSlice";
-import { clearData, storeData } from "../utils/asyncStorage";
-import { getData } from "../utils/asyncStorage";
+import { logoutFailed, logoutStart, logoutSuccess, loginFailed, loginStart, loginSuccess, registerFailed, registerStart, registerSuccess } from "./slices/authSlice";
+import { storeData } from "../utils/asyncStorage";
+import { ENV } from '../constants/env'
 
-const BASE_URL = "http://10.0.2.2:2001/api" // android studio
-// const BASE_URL = "http://10.0.3.2:2001/api" // genymotion
+const BASE_URL = ENV.BASE_URL + "/api"
 
 export const registerUser = async (user, dispatch) => {
     dispatch(registerStart())
@@ -26,32 +23,30 @@ export const registerUser = async (user, dispatch) => {
         console.log('register', JSON.stringify(json))
         dispatch(registerSuccess())
     }
-    catch(e) {
-        dispatch(registerFailed)
+    catch (e) {
+        dispatch(registerFailed())
     }
 }
 
 export const loginUser = async (user, dispatch) => {
     dispatch(loginStart())
-    try{
+    try {
         const res = await fetch(`${BASE_URL}/auth/login`, {
             method: "POST",
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({
-                username: user.username,
-                password: user.password
-            }),
+            body: JSON.stringify(user),
         })
         const json = await res.json()
+        console.log('login', json)
         storeData("@statusLogin", res.status)
         storeData("@userLogin", json)
         dispatch(loginSuccess(json))
     }
-    catch(e){
-        console.log(e)
+    catch (e) {
+        console.log("errorLoginUser", e)
         dispatch(loginFailed())
     }
 }
@@ -59,19 +54,20 @@ export const loginUser = async (user, dispatch) => {
 export const logoutUser = async (dispatch, accessToken, axiosJWT) => {
     dispatch(logoutStart())
     try {
-        const res = await axiosJWT.post(`${BASE_URL}/auth/logout`, {
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                Token: accessToken
-            }
-        })
-        const json = res.json()
-        console.log(json)
+        const res = await axiosJWT.post(`${BASE_URL}/auth/logout`,
+            {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Token: accessToken
+                }
+            })
+        console.log('logout', res.data)
         dispatch(logoutSuccess())
-        clearData()
+        dispatch(loginSuccess())
     }
     catch (e) {
+        console.log('errorLogout', e)
         dispatch(logoutFailed())
     }
 }
@@ -79,21 +75,19 @@ export const logoutUser = async (dispatch, accessToken, axiosJWT) => {
 export const updateUser = async (id, newUser, accessToken, dispatch, axiosJWT) => {
     dispatch(loginStart())
     try {
-        const res = await axiosJWT.put(`${BASE_URL}/user/${id}`, newUser, 
-        {
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                Token: accessToken
-            }
-        })
-        const json = await res.json()
-        console.log(JSON.stringify(json))
+        const res = await axiosJWT.put(`${BASE_URL}/user/${id}`, newUser,
+            {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Token: accessToken
+                }
+            })
+        console.log(JSON.stringify(res.data))
         dispatch(loginSuccess(newUser))
-        dispatch(updateUserSuccess())
     }
-    catch(e) {
-        console.log('4')
+    catch (e) {
+        console.log("errorUpdate", e)
         dispatch(loginFailed())
     }
 }
