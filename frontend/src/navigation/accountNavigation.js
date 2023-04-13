c2f60c5f2a434d3462cea92a95210abbd29e65a7
TrinhTrungVan <vantrinhtrung@gmail.com>
2023-04-13 13:10:28 +0700

    Arrow-down icon

---
+++ frontend/src/navigation/accountNavigation.js
@@ -0,0 +1,80 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+import { View, Text } from 'react-native'
+import ProfileScreen from '../screens/ProfileScreen'
+import SettingsScreen from '../screens/SettingsScreen'
+import { StyleSheet } from 'react-native'
+import COLORS from '../constants/color'
+
+const Stack = createNativeStackNavigator()
+
+const AccountNavigation = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name='Profile'
+                component={ProfileScreen}
+                options={{
+                    headerShown: true,
+                    header: () => (
+                        <View style={styles.header}>
+                            <Text style={styles.title}>Thông tin tài khoản</Text>
+                            <Text style={styles.cancelBtn}></Text>
+                        </View>
+                    ),
+                }}
+            />
+            <Stack.Screen
+                name='Settings'
+                component={SettingsScreen}
+                options={{
+                    headerShown: true,
+                    header: () => (
+                        <View style={styles.header}>
+                            <Text style={styles.title}>Cập nhật thông tin</Text>
+                            <Text style={styles.cancelBtn}></Text>
+                        </View>
+                    ),
+                }}
+            />
+        </Stack.Navigator>
+    )
+}
+
+export default AccountNavigation
+
+const styles = StyleSheet.create({
+    // shadow: {
+    //     shadowColor: "#7F5DF0",
+    //     shadowOffset: {
+    //         width: 0,
+    //         height: 10,
+    //     },
+    //     shadowOpacity: 0.25,
+    //     shadowRadius: 3.5,
+    //     elevation: 5,
+    // },
+    header: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 55,
+        marginTop: 24,
+        position: "relative",
+        borderBottomColor: COLORS.black,
+        borderBottomWidth: 0.5,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "500",
+        zIndex: 10,
+    },
+    cancelBtn: {
+        position: "absolute",
+        right: 24,
+    },
+    backBtn: {
+        position: "absolute",
+        left: 24,
+    },
+});
