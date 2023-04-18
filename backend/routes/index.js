f8e77e3a3525d75110c4fbf99a88b9aa308a9435
quyenptitptit <quyenmeomeo111@gmail.com>
2023-04-19 00:50:28 +0700

    api oauth2

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 import postRouter from "./postRouter.js";
 import authRouter from './authRoute.js';
 import userRouter from "./userRoute.js";
+import sendEmailRouter from "./sendEmailRoute.js";
 
 const route = (app) => {
     app.use("/api/auth", authRouter);
     app.use("/api/user", userRouter);
     app.use("/api/post", postRouter);
+    app.use("/api/email", sendEmailRouter)
 };
 
 export default route;
