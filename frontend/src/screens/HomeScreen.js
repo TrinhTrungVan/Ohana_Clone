72d4746d8ca347979823f9970433b934d6f920ab
cjsonjon <31162745+cjsonjon@users.noreply.github.com>
2023-04-18 22:39:17 +0700

    Dọn code thừa

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -22,101 +22,23 @@ import axios from "axios";
 import Post from "../components/Post";
 
 export default function HomeSearch({ navigation }) {
-    // const navigation = useNavigation();
     const [data, setData] = useState([]);
 
-    const [option, setOption] = useState();
     const [filteredData, setFilteredData] = useState([]);
 
-    // const url = "https://jsonplaceholder.typicode.com/posts";
 
     React.useEffect(() => {
-        // fetch(url)
-        //     .then((response) => response.json())
-        //     .then((json) => {
-        //         setData(json);
-        //         setmainData(json);
-        //     })
-        //     .catch((error) => {
-        //         console.error(error);
-        //     });
-        //console.log(priceRange)
         const getPost = async () => {
             const res = await postServices.getPosts();
             setData(res);
             setFilteredData(res);
-            //console.log(res);
         };
         getPost();
     }, [navigation]);
 
-    // const updateData = () => {
-    //     fetch(url)
-    //         .then((response) => response.json())
-    //         .then((json) => {
-    //             setData(json);
-    //         });
-    // };
 
-    const [isSearched, setIsSearched] = useState(false);
 
-    //All 3 scenario: Quas, Aut, Molestias
     function handleFilter(searchTerm) {
-        // var newData;
-        // if (searchTerm || (!searchTerm && idRange)) {
-        //     if (option == "title") {
-        //         // if (data.hasOwnProperty(key)) {
-        //         //     console.log(key + " -> " + data[key].title);
-        //         //     setData(
-        //         //         data.filter((item) =>
-        //         //             item.title.toUpperCase().includes(searchTerm.toUpperCase())
-        //         //         )
-        //         //     );
-
-        //         // }
-        //         var newData = filteredData.filter((item) =>
-        //             item.title.toUpperCase().includes(searchTerm.toUpperCase())
-        //         );
-        //     } 
-        //     // else if (option == "id") {
-        //     //     var newData = mainData.filter((item) =>
-        //     //         item.id.toString().toUpperCase().includes(searchTerm.toUpperCase())
-        //     //     );
-        //     // } 
-        //     else if (!option) {
-        //         setOption("title");
-        //         var newData = filteredData.filter((item) =>
-        //             item.title.toUpperCase().includes(searchTerm.toUpperCase())
-        //         );
-        //     }
-
-        //     if (idRange) {
-        //         if (idRange == 1) {
-        //             newData = newData.filter(
-        //                 (item) =>
-        //                     //item.id.toString().toUpperCase().includes(idRange.toUpperCase())
-        //                     item.id <= 10
-        //             );
-        //         } else if (idRange == 2) {
-        //             newData = newData.filter(
-        //                 (item) =>
-        //                     //item.id.toString().toUpperCase().includes(idRange.toUpperCase())
-        //                     item.id <= 20 && item.id > 10
-        //             );
-        //         } else if (idRange == 3) {
-        //             newData = newData.filter(
-        //                 (item) =>
-        //                     //item.id.toString().toUpperCase().includes(idRange.toUpperCase())
-        //                     item.id > 20
-        //             );
-        //         }
-
-        //         //setData(newData)
-        //     }
-        //     setData(newData);
-        // } else if (!searchTerm) {
-        //     updateData();
-        // }
         if (searchTerm != undefined
             || (searchTerm == undefined && seCity != null)) {
             var newData = [];
@@ -132,69 +54,35 @@ export default function HomeSearch({ navigation }) {
                     }
                 }
             }
-            //console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-            //console.log(newData);
-
-            //setFilteredData(newData);
             if (seCity.length != 0) {
-                // if (data[i].city != undefined && data[i].city.toUpperCase().includes(seCity.toUpperCase())) {
-                //     console.log(data[i].city)
-                //     //newData[i] = data[i]
-                // }
-                console.log("---------------------------------------------------------------")
-                // for (let i =0; i<newData.length; i++) {
-                //     console.log(newData[i].title + ", " +newData[i].city)
-                //     if (!newData[i].city.toUpperCase().includes(seCity.toUpperCase())) {
-                //         // console.log("Exist: \n" + newData[i].title + "; " +newData[i].city  + "\n")
-                //         //console.log(newData[i].title)
-                //         // var newstuff = newData[i];
-                //         // newData[i] = newData[newData.length-1];
-                //         // newData[newData.length-1] = newstuff;
-                //         console.log(", 0" )
-                //         //newData.pop()
-                //         //console.log(", 0" )
-                //         newData = 
-                //     } 
-                //     console.log("\n")
-                // }
                 newData = newData.filter(i => i.city.toUpperCase().includes(seCity.toUpperCase()))
-                //console.log(newData)
-                //setFilteredData(newData)
-                console.log(newData.length)
-                console.log("---------------------------------------------------------------")
-                console.log(seCity)
+                
             }
             if (seDistrict.length != 0) {
                 newData = newData.filter(i => i.district.toUpperCase().includes(seDistrict.toUpperCase()))
-                console.log('123123123123123123123213')
             }
             if (seWard.length != 0) {
                 newData = newData.filter(i => i.ward.toUpperCase().includes(seWard.toUpperCase()))
-                //console.log(seWard)
             }
             if (priceRange) {
                 if (priceRange == 1) {
                     newData = newData.filter(
                         (item) =>
-                            //item.id.toString().toUpperCase().includes(idRange.toUpperCase())
                             item.expenses < 1000000
                     );
                 } else if (priceRange == 2) {
                     newData = newData.filter(
                         (item) =>
-                            //item.id.toString().toUpperCase().includes(idRange.toUpperCase())
                             item.expenses < 2500000 && item.expenses >= 1000000
                     );
                 } else if (priceRange == 3) {
                     newData = newData.filter(
                         (item) =>
-                            //item.id.toString().toUpperCase().includes(idRange.toUpperCase())
                             item.expenses < 5000000 && item.expenses >= 2500000
                     );
                 } else if (priceRange == 4) {
                     newData = newData.filter(
                         (item) =>
-                            //item.id.toString().toUpperCase().includes(idRange.toUpperCase())
                             item.expenses >= 5000000
                     );
                 }
@@ -204,19 +92,7 @@ export default function HomeSearch({ navigation }) {
             console.log("\n")
             console.log(newData)
             console.log("\n")
-            // console.log(data.filter((item) => {
-            //     if (item.title != undefined && item.title.toUpperCase().includes(searchTerm.toUpperCase())) {
-            //         console.log("-----\n"+item.title +"\n-----\n")
-            //     }
-            // }))
-            //setFilteredData(newData)
-            //console.log(data[0].title)
-
-            // data.filter((item)=> {
-            //     if (item.title != undefined) {
-            //         console.log("-----\n"+item.title +"\n-----\n")
-            //     }
-            // });
+           
         } else {
             getPost()
         }
@@ -224,10 +100,6 @@ export default function HomeSearch({ navigation }) {
     }
 
     const [showModal, setShowModal] = useState(false);
-
-    const [option1, setOption1] = useState();
-    const [option2, setOption2] = useState();
-    const [option3, setOption3] = useState();
 
     const [searchInp, setSearchInp] = useState();
 
@@ -243,7 +115,6 @@ export default function HomeSearch({ navigation }) {
     const [seWard, setSeWard] = useState([]);
 
     const handleSelectCity = async (item) => {
-        // setLoading(true);
         setcData({ ...cdata, city: item.name, district: null, ward: null });
         await fetch(`https://provinces.open-api.vn/api/p/${item.code}?depth=2`)
             .then((res) =>
@@ -252,43 +123,27 @@ export default function HomeSearch({ navigation }) {
             )
             .then((res) => {
                 setDistrictList(res.districts)
-                //console.log(res)
             });
-        // setLoading(false);
-        //console.log(cdata.city)
-
+    
         setSeCity(item.name)
-        //console.log(cdata.city)
-        //console.log(seCity)
-
         setSeDistrict([])
         setSeWard([])
-        //handleFilter(undefined)
 
     };
 
     const handleSelectDistrict = async (item) => {
-        // setLoading(true);
         setcData({ ...cdata, district: item.name });
         await fetch(`https://provinces.open-api.vn/api/d/${item.code}?depth=2`)
             .then((res) => res.json())
             .then((res) => setWardList(res.wards));
-        // setLoading(false);
-        //console.log(cdata.district)
+    
         setSeDistrict(item.name)
         console.log(seDistrict)
     };
 
-    // const handleIdRange = async (item) => {
-    //     var newData = mainData.filter((item) =>
-    //         item.title.toUpperCase().includes(searchTerm.toUpperCase())
-    //     )
-    //     setData(newData);
-    // };
 
     const handleSelectWard = (item) => {
         setcData({ ...cdata, ward: item.name });
-        //console.log(cdata.ward);
         setSeWard(item.name)
         console.log(seWard)
 
@@ -296,19 +151,6 @@ export default function HomeSearch({ navigation }) {
 
     return (
         <SafeAreaView style={styles.container}>
-            {/* <ScrollView style={styles.pickerStyle} >
-                <Picker
-
-                    selectedValue={option}
-                    onValueChange={(itemValue, itemIndex) => {
-                        setOption(itemValue);
-                        console.log(option);
-                    }
-                    }>
-                    <Picker.Item label="Title" value="title" />
-                    <Picker.Item label="Id" value="id" />
-                </Picker>
-            </ScrollView> */}
             <TextInput
                 style={styles.textInputStyle}
                 onChangeText={(text) => {
@@ -316,7 +158,6 @@ export default function HomeSearch({ navigation }) {
                     setSearchInp(text)
                 }
                 }
-                //onChangeText={(text) => setSearchInp(text)}
                 value={data}
                 placeholder='Tìm kiếm'
             />
@@ -330,8 +171,6 @@ export default function HomeSearch({ navigation }) {
                         console.log("Modal has been closed.");
                     }}
                 >
-                    {/*All views of Modal*/}
-                    {/*Animation can be slide, slide, none*/}
                     <View style={styles.modal}>
                         <Button
                             title='Đóng bộ lọc'
